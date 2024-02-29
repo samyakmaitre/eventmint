78c78d2f5757ce0150d61dd8d343e3919ea03c74
samyakmaitre <pixelpirate404@gmail.com>
2024-02-29 16:12:23 +0530

    cvbn

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-from app import routes
