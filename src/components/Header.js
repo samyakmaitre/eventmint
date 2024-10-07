d61ddca239f5d0574dde883d3534807c22f58cd6
arnav <arnabdas2753@gmail.com>
2024-10-07 13:54:50 +0530

    Feature: Make Logo Clickable to Redirect to Home Page & also add seacrh icon #92

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,30 @@
-// Header.js
-import React from 'react'; // Add this line
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';  // Import the search icon
 import '../assets/styles/Header.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';  
-
 
 function Header() {
   return (
     <header className="header container-fluid d-flex align-items-center justify-content-between p-3 bg-light">
-      <div className="logo">
-        <span>book</span><span className="highlight">my</span><span>show</span>
-      </div>
+      <Link to="/" className="logo-link">
+        <div className="logo">
+          <span>book</span><br />
+          <span className="highlight">my</span><br />
+          <span>show</span>
+        </div>
+      </Link>
+      
       <div className="search-bar input-group">
         <input 
           type="text" 
           className="form-control" 
           placeholder="Search for Movies, Events, Plays, Sports and Activities" 
         />
+        <span className="input-group-text search-icon">
+          <FaSearch />  {/* Add search icon next to the input field */}
+        </span>
       </div>
+      
       <div className="location-signin d-flex align-items-center">
         <select className="location me-3 form-select">
           <option value="Nagpur">Nagpur</option>
