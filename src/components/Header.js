8fff12bda04f0dc3c9c1f4edea65084d2b64f11f
Samyak Maitre <130790017+samyakmaitre@users.noreply.github.com>
2024-10-07 16:47:07 +0530

    Revert "Feature: Make Logo Clickable to Redirect to Home Page & also add search icon"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,23 @@
-import React from 'react';
+// Header.js
+import React from 'react'; // Add this line
 import { Link } from 'react-router-dom';
 import '../assets/styles/Header.css';
-import { FaSearch } from 'react-icons/fa';  
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';  
+
 
 function Header() {
   return (
     <header className="header container-fluid d-flex align-items-center justify-content-between p-3 bg-light">
-      <Link to="/"  className="logo-link no-underline">
-        <div className="logo">
-          <span className="text-black p-1" >book</span>
-          <span className=" text-red-700 p-1">my</span>
-          <span className="text-black">show</span>
-        </div>
-      </Link>
-      
+      <div className="logo">
+        <span>book</span><span className="highlight">my</span><span>show</span>
+      </div>
       <div className="search-bar input-group">
         <input 
           type="text" 
           className="form-control" 
           placeholder="Search for Movies, Events, Plays, Sports and Activities" 
         />
-        <span className="input-group-text search-icon">
-          <FaSearch />
-        </span>
       </div>
-      
       <div className="location-signin d-flex align-items-center">
         <select className="location me-3 form-select">
           <option value="Nagpur">Nagpur</option>
