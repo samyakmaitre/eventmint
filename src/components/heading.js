4304f0123370114d97210759bf705af5a8d0372d
vanshika704 <sharmavanshi704@gmail.com>
2024-10-08 22:25:12 +0530

    navbar added

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import NavBar from './NavBar';
 
 function Heading() {
     return (
-        <div className="text-5xl font-extrabold mt-14 h-0 mb-0" style={{ fontFamily: 'Roboto' }}>
-            Book Now, <span style={{ color: 'red' }}>Skip the Line</span>, Vibe Later!
+        <div className="w-screen" style={{ fontFamily: 'Roboto' }}>
+           <NavBar/>
         </div>
     );
 }
