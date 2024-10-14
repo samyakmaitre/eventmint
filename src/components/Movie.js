a178d32cc7f840ef2d76fcdba4a3b1b85111bc97
Gauravtb2253 <gtbomble225@gmail.com>
2024-10-15 00:26:10 +0530

    solved em10

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import '../assets/styles/Movie.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../assets/styles/Movie.css";
+import { Link } from "react-router-dom";
 
 function Movie({ id, poster, title, rating, votes, genre }) {
   return (
     <div className="movie">
       <img src={poster} alt={`${title} poster`} className="movie-poster" />
-      <h2 className="movie-title">{title}</h2>
+      <span className="promoted">PROMOTED</span>
       <p className="movie-rating">
-        <span className="rating-star">★</span> {rating}/10 <span className="votes">({votes} Votes)</span>
+        <span className="rating-star">★</span>
+        <span className="rating-points"> {rating}/10 </span>
+        <span className="votes">({votes} Votes)</span>
       </p>
+      <h2 className="movie-title">{title}</h2>
       <p className="movie-genre">{genre}</p>
       <Link to={`/movies/${id}`}>
         <button className="book-tickets-button">More Details</button>
