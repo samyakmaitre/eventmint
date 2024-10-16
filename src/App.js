f341d4a22873b6d819bdee59be42eb8e8209f67a
siddharth A <siddhuplr@gmail.com>
2024-10-17 01:35:54 +0530

    Created StreamEvents.js and Plays.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,36 @@
 /** @format */
 
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom"; // Import Router from react-router-dom
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"; 
 import Home from "./components/Home";
 import Template from "./components/Auth/Template";
 import Forget from "./components/Forget";
-import MovieDetails from './components/MovieDetails';
+import MovieDetails from "./components/MovieDetails";
 import AboutUs from "./components/AboutUs";
 import Offers from "./components/Offers";
-
+import StreamEvents from "./components/StreamEvents";
+import Plays from "./components/Plays";
 
 function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          {<Route path="/login" element={<Template formType={"login"} />} />}
 
-	return (
-		<div>
-			<Router>
-				<Routes>
-					<Route
-						path="/"
-						element={<Home />}></Route>
-					{<Route
-						path="/login"
-						element={<Template formType={"login"} />}
-					/>}
-
-					<Route
-						path="/signup"
-						element={<Template formType={"signup"} />}
-					/>
-					<Route path="/forget" element={<Forget />} />
-					<Route path="/movies/:id" element={<MovieDetails />} />
-					<Route path="/about-us" element={<AboutUs />} />
-					<Route path="/offers" element={<Offers/>}/>
-				</Routes>
-			</Router>
-		</div>
-	);
-
+          <Route path="/signup" element={<Template formType={"signup"} />} />
+          <Route path="/forget" element={<Forget />} />
+          <Route path="/movies/:id" element={<MovieDetails />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/stream-events" element={<StreamEvents />} />
+          <Route path="/plays" element={<Plays />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
